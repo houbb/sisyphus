5c2c98d20436c484a7fbc3a9a568a3e6d980e24b
binbin.hou <1060732496@qq.com>
2019-05-22 11:00:10 +0800

    [Optimize] 优化单例

--- sisyphus-test/src/test/java/com/github/houbb/sispyhus/test/core/RetryerTest.java
+++ sisyphus-test/src/test/java/com/github/houbb/sispyhus/test/core/RetryerTest.java
@@ -1,7 +1,6 @@
 package com.github.houbb.sispyhus.test.core;
 
 import com.github.houbb.sisyphus.core.core.Retryer;
-import com.github.houbb.sisyphus.core.support.condition.RetryConditions;
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
@@ -12,15 +11,32 @@ import java.util.concurrent.Callable;
  */
 public class RetryerTest {
 
+    /**
+     * 不会触发重试
+     */
     @Test
-    public void helloTest() {
+    public void commonTest() {
         Retryer.<String>newInstance()
-                .condition(RetryConditions.isEqualsResult("fail"))
                 .retry(new Callable<String>() {
                     @Override
                     public String call() throws Exception {
                         System.out.println("called...");
-                        return "fail";
+                        return null;
+                    }
+                });
+    }
+
+    /**
+     * 默认异常进行重试
+     */
+    @Test
+    public void helloTest() {
+        Retryer.<String>newInstance()
+                .retry(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        System.out.println("called...");
+                        throw new RuntimeException();
                     }
                 });
     }

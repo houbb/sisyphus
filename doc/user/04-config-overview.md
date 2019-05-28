9fd9b2315c477f1e0699c38fdf1d159222045f4a
houbb <1060732496@qq.com>
2019-05-28 22:59:52 +0800

    [Feature] add for new

---
+++ doc/user/04-config-overview.md
@@ -0,0 +1,82 @@
+# 配置概览
+
+为了满足更加方便的配置，Retryer 类提供了许多可以配置的信息。
+
+## 默认配置
+
+```java
+/**
+ * 默认配置测试
+ */
+public void defaultConfigTest() {
+    Retryer.<String>newInstance()
+            .condition(RetryConditions.hasExceptionCause())
+            .retryWaitContext(RetryWaiter.<String>retryWait(NoRetryWait.class).context())
+            .maxAttempt(3)
+            .listen(RetryListens.noListen())
+            .recover(Recovers.noRecover())
+            .callable(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("called...");
+                    throw new RuntimeException();
+                }
+            }).retryCall();
+}
+```
+
+和下面的代码是等价的：
+
+```java
+public void helloTest() {
+    Retryer.<String>newInstance()
+            .callable(new Callable<String>() {
+                @Override
+                public String call() throws Exception {
+                    System.out.println("called...");
+                    throw new RuntimeException();
+                }
+            }).retryCall();
+}
+```
+
+# 方法说明
+
+## condition
+
+重试触发的条件，可以指定多个条件。
+
+默认为抛出异常。
+
+## retryWaitContext
+
+重试等待的策略，可以指定多个。
+
+默认为不做任何等待。 
+
+## maxAttempt
+
+指定最大重试次数，包括第一次执行。
+
+默认值：3 次。
+
+## listen
+
+指定重试的监听实现，默认为不做监听。
+
+## recover
+
+当重试完成之后，依然满足重试条件，则可以指定恢复的策略。
+
+默认不做恢复。
+
+## callable
+
+待重试执行的方法。
+
+## retryCall
+
+触发重试执行。
+
+# 接口的详细介绍
+
--- sisyphus-test/src/test/java/com/github/houbb/sispyhus/test/core/RetryerTest.java
+++ sisyphus-test/src/test/java/com/github/houbb/sispyhus/test/core/RetryerTest.java
@@ -1,6 +1,12 @@
 package com.github.houbb.sispyhus.test.core;
 
+import com.github.houbb.sisyphus.core.core.RetryWaiter;
 import com.github.houbb.sisyphus.core.core.Retryer;
+import com.github.houbb.sisyphus.core.support.condition.RetryConditions;
+import com.github.houbb.sisyphus.core.support.listen.RetryListens;
+import com.github.houbb.sisyphus.core.support.recover.Recovers;
+import com.github.houbb.sisyphus.core.support.wait.NoRetryWait;
+
 import org.junit.Test;
 
 import java.util.concurrent.Callable;
@@ -32,6 +38,26 @@ public class RetryerTest {
     @Test(expected = RuntimeException.class)
     public void helloTest() {
         Retryer.<String>newInstance()
+                .callable(new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        System.out.println("called...");
+                        throw new RuntimeException();
+                    }
+                }).retryCall();
+    }
+
+    /**
+     * 默认配置测试
+     */
+    @Test(expected = RuntimeException.class)
+    public void defaultConfigTest() {
+        Retryer.<String>newInstance()
+                .maxAttempt(3)
+                .listen(RetryListens.noListen())
+                .recover(Recovers.noRecover())
+                .condition(RetryConditions.hasExceptionCause())
+                .retryWaitContext(RetryWaiter.<String>retryWait(NoRetryWait.class).context())
                 .callable(new Callable<String>() {
                     @Override
                     public String call() throws Exception {

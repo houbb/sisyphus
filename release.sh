f7443704e42331cafb35dcd4fe98ed169f93325a
houbb <1060732496@qq.com>
2019-06-20 22:57:57 +0800

    release branch 0.0.6

--- release.sh
+++ release.sh
@@ -2,8 +2,8 @@
 echo "============================= RELEASE START..."
 
 ## 版本号信息(需要手动指定)
-version="0.0.1"
-newVersion="0.0.2"
+version="0.0.6"
+newVersion="0.0.7"
 projectName="sisyphus"
 
 # release 项目版本
